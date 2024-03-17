ee03291cb035f57ba4635b88d361134e6543d778
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-03-17 12:21:12 +0100

    Bloc section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import TopSection from "./components/TopSection/TopSection";
 import Advantages from "./components/Advantages/Advantages";
+import Bloc from "./components/BlocFolder/Bloc";
 
 function App() {
   return (
     <>
       <TopSection />
       <Advantages />
+      <Bloc />
     </>
   );
 }
---
+++ src/components/BlocFolder/Bloc.jsx
@@ -0,0 +1,50 @@
+import "./Bloc.css";
+
+export default function Bloc() {
+  return (
+    <section className="bloc">
+      <div className="bloc-container">
+        <span className="bloc-column">
+          <h2 className="bloc-column-title">Lorem ipsum dolor sit</h2>
+          <h4 className="bloc-column-subtitle">
+            Lorem ipsum dolor sit amet consectetur.
+          </h4>
+        </span>
+        <span className="bloc-column">
+          <p className="bloc-column-paragraph">
+            Lorem ipsum dolor sit amet consectetur. Erat neque viverra pharetra
+            magna risus donec diam habitant molestie. Enim vestibulum nam
+            elementum urna nunc molestie semper vitae aliquam. Elit justo vitae
+            eu maecenas nec elementum. Rutrum aenean porttitor auctor nisl ipsum
+            sit diam sed nibh. Sollicitudin nullam imperdiet erat diam bibendum
+            sit elementum. Eget consectetur parturient convallis feugiat nunc.
+            Dolor tellus lectus lorem egestas. Vitae in mi posuere est. Odio sit
+            vitae nisi in eros. Aliquet venenatis malesuada mus in quam in.
+            Tempor enim porta tincidunt erat tempus integer eget et. Lectus
+            libero pretium cursus faucibus eu ornare amet dolor. Id elit ut sit
+            leo morbi. Sit lacus praesent venenatis mi enim senectus.
+          </p>
+          <p className="bloc-column-paragraph">
+            {" "}
+            Proin pulvinar pharetra congue quis mauris. Neque vitae molestie a
+            ac velit malesuada. Amet a elementum nisl felis. A cras et habitant
+            mi amet eu amet. Nec non scelerisque diam ac dapibus commodo amet
+            morbi sed. Sit pellentesque diam id tempor in hac feugiat lacus.
+            Tempus lectus accumsan commodo sagittis faucibus nibh elementum
+            gravida. Eget morbi sed quisque senectus pharetra. Cursus eget
+            aliquam ullamcorper fringilla convallis venenatis viverra sed.
+            Placerat nam sit proin ut lorem cras aliquet.
+          </p>
+          <p className="bloc-column-paragraph">
+            Dui viverra dui magna ultrices cras egestas nec sed. Mollis vel
+            egestas pretium egestas. Semper mauris elit ac pretium. Dis sagittis
+            eu massa id neque massa id velit urna. Lacus ipsum aliquet dui
+            tempor magnis urna sed pellentesque. Mauris id massa posuere euismod
+            nisl in mi in proin. Est tortor mattis pulvinar a. Egestas convallis
+            integer sed turpis.
+          </p>
+        </span>
+      </div>
+    </section>
+  );
+}
