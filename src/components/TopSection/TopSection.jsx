682679e6c18ec16094bd724ece000b9f772bcd80
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-03-17 17:22:31 +0100

    Top section functionality added

--- src/components/TopSection/TopSection.jsx
+++ src/components/TopSection/TopSection.jsx
@@ -2,8 +2,20 @@ import "./TopSection.css";
 import SvgArrow from "../../SvgComponents/SvgArrow";
 import SvgColillasLogo from "../../SvgComponents/SvgColillasLogo";
 import backgroundImage from "../../assets/leone-venter-VieM9BdZKFo-unsplash.jpg";
+import { useState } from "react";
 
 export default function TopSection() {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  function flip(e) {
+    const arrow = e.currentTarget;
+    const article = arrow.closest(".selector-card");
+    if (article) {
+      article.classList.toggle("flipped");
+      setIsFlipped((state) => !state);
+    }
+  }
+
   return (
     <section className="top-section">
       <nav>
@@ -24,25 +36,25 @@ export default function TopSection() {
           <h4 className="selector-header-text">Lorem ipsum dolor sit amet</h4>
         </div>
         <hr className="selector-line" />
-        <article className="selector-card">
+        <article className={`selector-card ${isFlipped ? "flipped" : ""}`}>
           <h3 className="selector-card-text">
             Lorem ipsum dolor sit amet consectetur. Nibh vestibulum eleifend a
             et.
           </h3>
           <div className="selector-card-button">
-            <div className="arrow-image">
+            <div className="arrow-image" onClick={flip}>
               <SvgArrow />
             </div>
           </div>
         </article>
         <hr className="selector-line" />
-        <article className="selector-card">
+        <article className={`selector-card ${isFlipped ? "flipped" : ""}`}>
           <h3 className="selector-card-text">
             Lorem ipsum dolor sit amet consectetur. Amet sed vitae urna egestas
             amet.
           </h3>
           <div className="selector-card-button">
-            <div className="arrow-image">
+            <div className="arrow-image" onClick={flip}>
               <SvgArrow />
             </div>
           </div>
